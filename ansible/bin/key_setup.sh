d9ec7f5bd023b35c50f98a120ee15b52481e8d22
Korben Kirscht <korben@kirscht.com>
2020-07-01 16:29:27 -0700

    Update

---
+++ ansible/bin/key_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export AUTH_KEYS=~/.ssh/authorized_keys
+export ENDORE_KEY=/.keys/id_endore
+
+ls ${AUTH_KEYS} || touch ${AUTH_KEYS} && chmod 0600 ${AUTH_KEYS}
+
+ls ${ENDORE_KEY} || ssh-keygen -f ${ENDORE_KEY}
+ls ${ENDORE_KEY}.pub && cp ${AUTH_KEYS} /.keys/. && \
+  cat ${ENDORE_KEY}.pub >> /.keys/authorized_keys && \
+  sort -u /.keys/authorized_keys > ${AUTH_KEYS}
