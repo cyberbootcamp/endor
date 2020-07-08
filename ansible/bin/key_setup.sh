f741e3f1a0753e83b558802bbdd3a77bc1f943f5
Korben Kirscht <korben@kirscht.com>
2020-07-07 20:29:28 -0700

    Update Endor I

--- ansible/bin/key_setup.sh
+++ ansible/bin/key_setup.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 export AUTH_KEYS=~/.ssh/authorized_keys
-export ENDOR_KEY=/.keys/id_endor
+export ENDOR_KEY=~/.ssh/id_endor
 
 ls ~/.ssh || mkdir -p ~/.ssh/ && chmod 0700 ~/.ssh
 
 ls ${AUTH_KEYS} || touch ${AUTH_KEYS} && chmod 0600 ${AUTH_KEYS}
 
 ls ${ENDOR_KEY} || ssh-keygen -f ${ENDOR_KEY}
-ls ${ENDOR_KEY}.pub && cp ${AUTH_KEYS} /.keys/. && \
-  cat ${ENDOR_KEY}.pub >> /.keys/authorized_keys && \
-  sort -u /.keys/authorized_keys > ${AUTH_KEYS}
+ls ${ENDOR_KEY}.pub && cat ${ENDOR_KEY}.pub >> ${AUTH_KEYS} && \
+  sort -u ${AUTH_KEYS} > ${AUTH_KEYS}
